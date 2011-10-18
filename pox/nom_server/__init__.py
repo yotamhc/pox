882a0f8d384eb65a52381f9482402e97ca44a93c
Colin Scott <cs@cs.berkeley.edu>
2011-10-18 16:14:14 -0700

    Adding nom_trap.py: a "black-box" tester for harvesting invariants from controller applications

--- pox/nom_server/__init__.py
+++ pox/nom_server/__init__.py
@@ -1,6 +1,8 @@
 def launch ():
   import nom_server
+  import nom_trap
   import cached_nom
   from pox.core import core
-  core.registerNew(nom_server.NomServer)
-  # NomServer()
+  # TODO: how do I invoke NomTrap from pox.py?
+  #core.registerNew(nom_server.NomServer)
+  core.registerNew(nom_trap.NomTrap)
