75a57e0289d9eadf6e0a0948527b44e010383e3d
Colin Scott <cs@cs.berkeley.edu>
2011-11-27 02:03:37 -0800

    fuzzer: adding a comment.
    Realized that we don't really want to mock out network entities.... What we
    should really be doing is booting OpenV Switches on MiniNet, and feeding them
    randomly generated or trace-derived traffic...

--- pox/nom_trap/fuzzer_entities.py
+++ pox/nom_trap/fuzzer_entities.py
@@ -1,3 +1,16 @@
+"""
+This module mocks out openflow switches. 
+
+It just occurred to me that we don't really want to mock out network elements...
+Ultimately, our goal is to test the entire stack! So really, we should be using 
+MiniNet to boot open v switches, and then inject randomly generated traffic to the 
+switches themselves. This brings up two questions:
+  - Is it possible to package a MiniNet VM as part of POX? That would
+    go against the grain of POX's python-only, pre-packaged everything
+    philosophy.
+  - Can this module successfully interpose on all messages between vswitches
+    and the control application?
+"""
 
 from pox.openflow.topology import *
 from pox.openflow.of_01 import Connection
@@ -71,6 +84,7 @@ class MockOpenFlowSwitch (OpenFlowSwitch):
       log.warn("Switch already up")
     self.failed = False
     self.connection.reconnect()
+    
     
 class Message (object):
   """ So we can track a message throughout the network """
