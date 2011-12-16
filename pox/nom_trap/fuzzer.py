0f6876ee12210922774d5876b085d451ae1e11db
Colin Scott <cs@cs.berkeley.edu>
2011-12-16 00:58:53 -0800

    nom_trap: Adding anteater support.
    Right now it only creates binary code files. I haven't figured
    out what to do with the files.... Emailed the authors of the paper about it.

--- pox/nom_trap/fuzzer.py
+++ pox/nom_trap/fuzzer.py
@@ -17,6 +17,7 @@ from pox.openflow.libopenflow_01 import ofp_action_output
 import pox.nom_trap.default_topology as default_topology
 from fuzzer_entities import *
 from event_generator import EventGenerator
+from invariant_checker import InvariantChecker
 
 import sys
 import threading
@@ -26,6 +27,7 @@ import socket
 import time
 import random
 import os
+
 
 log = core.getLogger()
 
@@ -86,6 +88,7 @@ class FuzzTester (Topology):
       self.seed = 0.0
       self.random = random.Random(self.seed)
       self.event_generator = EventGenerator(self.random)
+      self.invariant_checker = InvariantChecker(self)
       
       # TODO: future feature: log all events, and allow user to (interactively)
       # replay the execution
@@ -149,6 +152,7 @@ class FuzzTester (Topology):
         self.logical_time += 1
         self.trigger_events()
         print("Round %d completed." % self.logical_time)
+        self.invariant_check_prompt()
         answer = raw_input('Continue to next round? [Yn]')
         if answer != '' and answer.lower() != 'y':
           self.stop()
@@ -271,73 +275,29 @@ class FuzzTester (Topology):
           handler = handlers[0][1]
           handler(event) 
           
+    def invariant_check_prompt(self):
+      answer = raw_input('Check Invariants? [Ny]')
+      if answer != '' and answer.lower() != 'n':
+          print "Which one?"
+          print "  'l' - loops"
+          print "  'b' - blackholes"
+          print "  'r' - routing consistency"
+          print "  'c' - connectivity"
+          answer = raw_input()
+          if answer.lower() == 'l':
+            self.invariant_checker.check_loops()
+          elif answer.lower() == 'b':
+            self.invariant_checker.check_blackholes()
+          elif answer.lower() == 'r':
+            self.invariant_checker.check_routing_consistency()
+          elif answer.lower() == 'c':
+            self.invariant_checker.check_connectivity()
+          else:
+            print "Unknown input..."
+          
     # TODO: do we need to define more event types? e.g., packet delivered,
     # controller crashed, etc. An alternative might be to just generate
     # sensible traffic, and let the switch raise its own events.
-    
-    def dump_csv(self):
-      """
-      For each switch in the network, dump it's Flow Table in csv format, 
-      for consumption by Anteater
-      
-      Returns a hash from switch_impl -> csv output
-      """
-      switch_impl2csv = {}
-      def switch_csv(switch):
-        """ Dump the Flow Table for a single switch """
-        # TODO: implement this as a method on SwitchImpl! Will need a mapping
-        #       of port -> next hop IP address
-        #
-        #       Only problem with putting it in SwitchImpl is that port -> next hop IP
-        #       assumes that the switch works at Layer 3...
-        lines = []
-        for entry in switch.table._table:
-          match, _, actions = entry 
-           
-          dst, prefix = match.get_nw_dst()
-          if dst is None:
-            dst = "0.0.0.0"
-          full_dst = '/'.join((str(dst), str(prefix)))
-          
-          # Default drop (no output action specified)
-          output_interface = "drop"
-          gateway = "DIRECT"
-          
-          for action in actions:
-            # TODO: assume that there is only one ofp_action_output in the list of actions?
-            if type(action) == ofp_action_output:
-              port_no = action.port
-              port = switch.ports[port_no]
-              port_name = port.name
-              if port_name == "":
-                port_name = "eth%d" % port_no
-              output_interface = port_name
-              gateway = switch.outgoing_links[port].end_port.ip_addr
-                      
-          # TODO: figure out what tags are used for in Anteater
-          tags = "O"
-      
-          csv = ','.join((full_dst, gateway, output_interface, tags))
-          lines.append(csv)
-          
-        # Now manually add in loopback devices
-        for port in switch.ports.values():
-          csv = "%s/32,DIRECT,loopback 1,O" % str(port.ip_addr)
-          lines.append(csv)
-        
-        # TODO: problem! Anteater assumes destination-based routing. OpenFlow is
-        # (source, destination)-based routing.
-        return "\n".join(lines)
-      
-      # Now, get a csv string for each switch
-      for switch in self.getEntitiesOfType(MockOpenFlowSwitch):
-        # TODO: don't assume that the switch has a reference to the implementation! For emulation, will need 
-        #       to fetch table of the implementation through a more realistic means
-        switch_impl = switch.switch_impl
-        csv_output = switch_csv(switch_impl)
-        switch_impl2csv[switch_impl] = csv_output
-      
-      return switch_impl2csv  
-        
+       
 if __name__ == "__main__":
   pass
---
+++ pox/nom_trap/invariant_checker.py
@@ -0,0 +1,147 @@
+
+from pox.core import core
+from pox.openflow.libopenflow_01 import * 
+from fuzzer_entities import *
+  
+import xml.etree.ElementTree as ET 
+import os
+
+logger = core.getLogger()
+
+class InvariantChecker():
+  def __init__(self, topology):
+    self.topology = topology
+    # TODO: don't hardcode!
+    self.jruby_path =  "/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/utils/jruby-1.6.5/bin/jruby"
+    self.library_path = "/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/build/lib/Ruby"
+    self.loop_detector = "/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/src/tools/loop-detector.rb"
+    self.consistency_detector = "/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/src/tools/consistency-checker.rb"
+    self.blackhole_detector = "/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/src/tools/packet_loss.rb"
+    # TODO: where is the connectedness detector script?
+    self.connectivity_detector = None
+    
+    # Set up environment variables
+    os.environ['ANTEATER_BUILD_DIR']="/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/build"
+    os.environ['ANTEATER_SRC_DIR']="/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/src"
+    os.environ['LLVM_BIN_DIR']="/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/dist/bin"
+    os.environ['LD_LIBRARY_PATH']="/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/build/lib/Core:$LD_LIBRARY_PATH"
+    os.environ['JRUBY_OPTS']="--server --1.9 -J-Xmx16384m -J-Djruby.compile.fastest=true -J-Djruby.compile.frameless=true -J-Djruby.compile.positionless=true -J-Djruby.compile.fastops=true -J-Djruby.compile.fastcase=true -J-Djruby.compile.lazyHandles=true"
+    os.environ['RUBY_EXECUTABLE']="/Users/rcs/Research/UCB/pox-debugger/pox/lib/anteater/utils/jruby-1.6.5/bin/jruby"
+    
+  # --------------------------------------------------------------#
+  #                    Invariant checks                           #
+  # --------------------------------------------------------------#
+  def check_loops(self):
+    self._run_anteater_script(self.loop_detector)
+    
+  def check_blackholes(self):
+    self._run_anteater_script(self.blackhole_detector)
+    
+  def check_connectivity(self):
+    self._run_anteater_script(self.connectivity_detector)
+  
+  def check_routing_consistency(self):
+    self._run_anteater_script(self.consistency_detector)
+    
+  def _run_anteater_script(self, script):
+    log.debug("Snapshotting FIBs...")
+    fib_manifest = self.generate_fib_manifest() 
+    cmd = ' '.join((self.jruby_path, '-I', self.library_path, script, fib_manifest))
+    log.debug("Running Anteater...")
+    #current_dir = os.getcwd()
+    #log.debug("leaving %s" % current_dir)
+    #os.chdir("/tmp")
+    #log.debug("now in %s" % os.getcwd())
+    log.debug(cmd)
+    os.system(cmd)
+    #os.chdir(current_dir)
+    # TODO: rm *fib manifest.xml *bc
+    
+  # --------------------------------------------------------------#
+  #                    FIB Snapshot                               #
+  # --------------------------------------------------------------#
+  def dump_csv(self):
+    """
+    For each switch in the network, dump it's Flow Table in csv format, 
+    for consumption by Anteater
+    
+    Returns a hash from switch_impl -> csv output
+    """
+    switch_impl2csv = {}
+    
+    def switch_csv(switch):
+      """ Dump the Flow Table for a single switch """
+      # TODO: implement this as a method on SwitchImpl! Will need a mapping
+      #       of port -> next hop IP address
+      #
+      #       Only problem with putting it in SwitchImpl is that port -> next hop IP
+      #       assumes that the switch works at Layer 3...
+      lines = []
+      for entry in switch.table._table:
+        match, _, actions = entry 
+         
+        dst, prefix = match.get_nw_dst()
+        if dst is None:
+          dst = "0.0.0.0"
+        full_dst = '/'.join((str(dst), str(prefix)))
+        
+        # Default drop (no output action specified)
+        output_interface = "drop"
+        gateway = "DIRECT"
+        
+        for action in actions:
+          # TODO: assume that there is only one ofp_action_output in the list of actions?
+          if type(action) == ofp_action_output:
+            port_no = action.port
+            port = switch.ports[port_no]
+            port_name = port.name
+            if port_name == "":
+              port_name = "eth%d" % port_no
+            output_interface = port_name
+            gateway = switch.outgoing_links[port].end_port.ip_addr
+                    
+        # TODO: figure out what tags are used for in Anteater
+        tags = "O"
+    
+        csv = ','.join((full_dst, gateway, output_interface, tags))
+        lines.append(csv)
+        
+      # Now manually add in loopback devices
+      for port in switch.ports.values():
+        csv = "%s/32,DIRECT,loopback 1,O" % str(port.ip_addr)
+        lines.append(csv)
+      
+      # TODO: problem! Anteater assumes destination-based routing. OpenFlow is
+      # (source, destination)-based routing.
+      return "\n".join(lines)
+    
+    # Now, get a csv string for each switch
+    for switch in self.topology.getEntitiesOfType(MockOpenFlowSwitch):
+      # TODO: don't assume that the switch has a reference to the implementation! For emulation, will need 
+      #       to fetch table of the implementation through a more realistic means
+      switch_impl = switch.switch_impl
+      csv_output = switch_csv(switch_impl)
+      switch_impl2csv[switch_impl] = csv_output
+    
+    return switch_impl2csv  
+  
+  def generate_fib_manifest(self):
+    """ Generate an XML manifest file for Anteater consumption """
+    switch_impl2csv = self.dump_csv()
+    root = ET.Element("manifest")
+    data = ET.SubElement(root, "data")
+    for switch in switch_impl2csv.keys():
+      node = ET.SubElement(data, "node")
+      node.set("name", switch.name)
+      fib_output_name = "%s.fib" % switch.name
+      node.set("file", fib_output_name)
+      fib_csv = switch_impl2csv[switch] 
+      log.debug("writing out %s..." % fib_output_name)
+      f = open(fib_output_name, 'w')
+      f.write(fib_csv)
+      f.close()
+      
+    tree = ET.ElementTree(root)   
+    output_name = "manifest.xml"
+    tree.write(output_name)
+    return output_name
