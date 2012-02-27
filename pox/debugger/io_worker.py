c59d8cbabbdeb9b3243dfcabc876d2869c4e6e06
Colin Scott <cs@cs.berkeley.edu>
2012-02-26 21:33:40 -0800

    pox.debugger: adding DeferredIOWorker. (with unit tests)

--- pox/debugger/io_worker.py
+++ pox/debugger/io_worker.py
@@ -22,7 +22,7 @@ class IOWorker(object):
     self.send_buf = ""
     self.receive_buf = ""
     self._on_data_receive = lambda worker: None
-
+    
   def set_receive_handler(self, block):
     self._on_data_receive = block
 
@@ -32,7 +32,7 @@ class IOWorker(object):
     self.send_buf += data
 
   def push_receive_data(self, new_data):
-    """ notify client of new received data. """
+    """ notify client of new received data. called by a Select loop """
     self.receive_buf += new_data
     self._on_data_receive(self)
 
@@ -59,7 +59,6 @@ class RecocoIOWorker(IOWorker):
   """ An IOWorker that works with our RecocoIOLoop, and notifies it via pinger """
   def __init__(self, socket, pinger):
     IOWorker.__init__(self)
-    # list of (potentially partial) messages to send
     self.socket = socket
     self.pinger = pinger
 
@@ -69,7 +68,73 @@ class RecocoIOWorker(IOWorker):
   def send(self, data):
     IOWorker.send(self, data)
     self.pinger.ping()
+    
+class DeferredIOWorker(object):
+  '''
+  Wrapper class for RecocoIOWorkers.
+  
+  Rather than actually sending/receiving right away, queue the messages.
+  Then there are separate methods for actually sending the messages via
+  the wrapped io_worker
+  '''
+  def __init__(self, io_worker):
+    self.io_worker = io_worker
+    # Thread-safe read and write queues of indefinite length
+    self.receive_queue = Queue()
+    self.send_queue = Queue()
+  
+  def permit_send(self):
+    '''
+    deque()s the first element of the write queue, and actually sends it
+    across the wire.
+    
+    raises an exception if the write queue is empty
+    '''
+    message = self.send_queue.get()
+    # TODO: will recoco guarentee in-order delivery of a sequence of these send requests?
+    # TODO: do I need to ensure thread-safety? We're crossing thread boundaries here...
+    self.io_worker.send(message)
+    
+  def send(self, data):
+    """ send data from the client side. fire and forget. """
+    self.send_queue.put(data)
+  
+  def permit_receive(self):
+    data = self.receive_queue.get()
+    self.io_worker.push_receive_data(data)
+  
+  def push_receive_data(self, new_data):
+    ''' called from the Select loop '''
+    self.receive_queue.put(new_data)
+  
+  # Delegation functions.
+  # TODO: is there a more pythonic way to implement delegation?
+  
+  @property
+  def ready_to_send(self):
+    ''' Called by Select loop to see if we're actually ready to send '''
+    # We only push write data onto the worker after we've permitted it.
+    return self.io_worker.ready_to_send
+  
+  def set_receive_handler(self, block):
+    self.io_worker.set_receive_handler(block)
+  
+  def fileno(self):
+    return self.io_worker.fileno()
+  
+  def close(self):
+    return self.io_worker.close()
+  
+  def peek_receive_buf(self):
+    return self.io_worker.peek_receive_buf()
 
+  def consume_receive_buf(self, l):
+    """ called from the client to consume receive buffer """
+    return self.io_worker.consume_receive_buf(l)
+  
+  def consume_send_buf(self, l):
+    return self.io_worker.consume_send_buf(l)
+    
 class RecocoIOLoop(Task):
   """
   recoco task that handles the actual IO for our IO workers
@@ -87,6 +152,11 @@ class RecocoIOLoop(Task):
     self.workers.add(worker)
     self.pinger.ping()
     return worker
+  
+  def create_deferred_worker_for_socket(self, socket):
+    real_worker = self.create_worker_for_socket(socket)
+    deferred_worker = DeferredIOWorker(real_worker)
+    return deferred_worker
 
   def stop(self):
     self.running = False
